12f483d84ec9925a41ac50ef2d6897ba25428c97
Tom White <eugene.white@gmail.com>
2019-08-31 03:30:52 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.each do |artist|
+    self.artists.collect do |artist|
       artist.name
     end
   end
